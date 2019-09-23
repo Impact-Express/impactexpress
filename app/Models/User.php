a9efa372b0cc90df884f35725e81c9997f875e97
Richard Bailey <richard@richardbailey.xyz>
2019-09-23 09:16:02 +0000

    added admin middleware to kernel

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,15 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
+    ]
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 
     public function getSalesTariffs() {
@@ -77,7 +86,7 @@ class User extends Authenticatable
     public function isAdmin() {
         return $this->group->name == 'admin';
     }
-    
+
     /**
      * Define relationships
      */
