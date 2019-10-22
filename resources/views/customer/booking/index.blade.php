2620a0ae24a6e3eb66c4c5d384f2cf6cd5613fbb
Richard Bailey <richard@impactexpress.co.uk>
2019-10-22 15:07:10 +0000

    dhl hermes address format

--- resources/views/customer/booking/index.blade.php
+++ resources/views/customer/booking/index.blade.php
@@ -7,7 +7,7 @@
 		<ul class="fieldlist date">
 			<li>
 				<label for="date">Date</label>
-				<input id="date" type="text" name="date" class="k-textbox" value="2019-08-30">
+				<input id="date" type="text" name="date" class="k-textbox" value="2019-10-23">
 			</li>
 		</ul>
 
